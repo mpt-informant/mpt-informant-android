933a75b42f4dd1e869c2984108c233d0b2f698df
kof3st <xkof3st.mail@gmail.com>
2022-11-01 20:11:18 +0300

    Авторефакторинг кода

--- app/src/main/java/me/kofesst/android/mptinformant/ui/ResourceString.kt
+++ app/src/main/java/me/kofesst/android/mptinformant/ui/ResourceString.kt
@@ -3,12 +3,12 @@ package me.kofesst.android.mptinformant.ui
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import java.text.SimpleDateFormat
+import java.util.*
 import me.kofesst.android.mptinformant.R
 import me.kofesst.android.mptinformant.presentation.views.GroupInfoViewTab
 import me.kofesst.android.mptinformer.domain.models.DayOfWeek
 import me.kofesst.android.mptinformer.domain.models.WeekLabel
-import java.text.SimpleDateFormat
-import java.util.*
 
 class ResourceString private constructor(@StringRes private val resId: Int) {
     companion object {
@@ -48,33 +48,33 @@ class ResourceString private constructor(@StringRes private val resId: Int) {
 
 @Composable
 fun WeekLabel.uiText(): String = (
-        when (this) {
-            WeekLabel.Numerator -> ResourceString.numeratorLabel
-            WeekLabel.Denominator -> ResourceString.denominatorLabel
-            WeekLabel.None -> ResourceString.appName
-        }
-        ).asString()
+    when (this) {
+        WeekLabel.Numerator -> ResourceString.numeratorLabel
+        WeekLabel.Denominator -> ResourceString.denominatorLabel
+        WeekLabel.None -> ResourceString.appName
+    }
+    ).asString()
 
 @Composable
 fun DayOfWeek.uiText(): String = (
-        when (this) {
-            DayOfWeek.Sunday -> ResourceString.sunday
-            DayOfWeek.Monday -> ResourceString.monday
-            DayOfWeek.Tuesday -> ResourceString.tuesday
-            DayOfWeek.Wednesday -> ResourceString.wednesday
-            DayOfWeek.Thursday -> ResourceString.thursday
-            DayOfWeek.Friday -> ResourceString.friday
-            DayOfWeek.Saturday -> ResourceString.saturday
-        }
-        ).asString()
+    when (this) {
+        DayOfWeek.Sunday -> ResourceString.sunday
+        DayOfWeek.Monday -> ResourceString.monday
+        DayOfWeek.Tuesday -> ResourceString.tuesday
+        DayOfWeek.Wednesday -> ResourceString.wednesday
+        DayOfWeek.Thursday -> ResourceString.thursday
+        DayOfWeek.Friday -> ResourceString.friday
+        DayOfWeek.Saturday -> ResourceString.saturday
+    }
+    ).asString()
 
 @Composable
 fun GroupInfoViewTab.uiText(): String = (
-        when (this) {
-            GroupInfoViewTab.Schedule -> ResourceString.schedule
-            GroupInfoViewTab.Changes -> ResourceString.changes
-        }
-        ).asString()
+    when (this) {
+        GroupInfoViewTab.Schedule -> ResourceString.schedule
+        GroupInfoViewTab.Changes -> ResourceString.changes
+    }
+    ).asString()
 
 @Composable
 fun Long.dateUiText(): String = with(
