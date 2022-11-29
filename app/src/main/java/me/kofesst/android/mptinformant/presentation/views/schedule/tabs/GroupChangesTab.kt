288b85b50b4b5ff6b1a5a9c6a27566d3c79280e6
kofesst <xkof3st.mail@gmail.com>
2022-11-29 17:16:21 +0300

    Элементы вкладок расписания и замен вынесены в отдельные файлы

---
+++ app/src/main/java/me/kofesst/android/mptinformant/presentation/views/schedule/tabs/GroupChangesTab.kt
@@ -0,0 +1,54 @@
+package me.kofesst.android.mptinformant.presentation.views.schedule.tabs
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mood
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import me.kofesst.android.mptinformant.domain.models.changes.GroupChanges
+import me.kofesst.android.mptinformant.presentation.utils.SuspendValue
+import me.kofesst.android.mptinformant.ui.ResourceString
+import me.kofesst.android.mptinformant.ui.components.GroupChangesColumn
+import me.kofesst.android.mptinformant.ui.components.IconMessage
+import me.kofesst.android.mptinformant.ui.components.SuspendValueHandler
+
+@Composable
+fun GroupChangesTab(
+    modifier: Modifier = Modifier,
+    changes: SuspendValue<GroupChanges>,
+    onChangesRefresh: () -> Unit,
+) {
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(
+            isRefreshing = changes.state == SuspendValue.State.Loading
+        ),
+        onRefresh = onChangesRefresh,
+        modifier = modifier
+    ) {
+        SuspendValueHandler(
+            value = changes
+        ) {
+            when {
+                it.days.isEmpty() -> {
+                    IconMessage(
+                        icon = Icons.Outlined.Mood,
+                        iconTint = MaterialTheme.colorScheme.onBackground,
+                        message = ResourceString.emptyChanges.asString(),
+                        messageStyle = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                else -> {
+                    GroupChangesColumn(
+                        changes = it,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/me/kofesst/android/mptinformant/presentation/views/schedule/tabs/GroupScheduleTab.kt
@@ -0,0 +1,55 @@
+package me.kofesst.android.mptinformant.presentation.views.schedule.tabs
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mood
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import me.kofesst.android.mptinformant.domain.models.schedule.GroupSchedule
+import me.kofesst.android.mptinformant.presentation.utils.SuspendValue
+import me.kofesst.android.mptinformant.ui.ResourceString
+import me.kofesst.android.mptinformant.ui.components.GroupScheduleColumn
+import me.kofesst.android.mptinformant.ui.components.IconMessage
+import me.kofesst.android.mptinformant.ui.components.SuspendValueHandler
+
+@Composable
+fun GroupScheduleTab(
+    modifier: Modifier = Modifier,
+    schedule: SuspendValue<GroupSchedule>,
+    onScheduleRefresh: () -> Unit,
+) {
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(
+            isRefreshing = schedule.state == SuspendValue.State.Loading
+        ),
+        onRefresh = onScheduleRefresh,
+        modifier = modifier
+    ) {
+        SuspendValueHandler(
+            value = schedule,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            when {
+                it.days.isEmpty() -> {
+                    IconMessage(
+                        icon = Icons.Outlined.Mood,
+                        iconTint = MaterialTheme.colorScheme.onBackground,
+                        message = ResourceString.emptySchedule.asString(),
+                        messageStyle = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                else -> {
+                    GroupScheduleColumn(
+                        schedule = it,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+}
